3d1b0da9e84aa9bc40523e883889fbd08f5cd411
Nick Haren <nickharen@gmail.com>
2018-06-05 10:39:15 -0400

    BitHelpers.To<T>() for converting bytes to unmanaged values

--- HotLib/Bits/BitHelpers.cs
+++ HotLib/Bits/BitHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace HotLib.Bits
@@ -181,6 +182,183 @@ namespace HotLib.Bits
                     stringBuilder.Append(bit == 0 ? '0' : '1');
                 }
             }
+        }
+
+        /// <summary>
+        /// Converts a series of bytes into an unmanaged value.
+        /// </summary>
+        /// <typeparam name="T">The unmanaged type to convert the bytes to.</typeparam>
+        /// <param name="bytes">The bytes to convert. Will be taken starting from the beginning of the span.</param>
+        /// <param name="endianness">The endianness of the given bytes. If it doesn't match the system's endianness,
+        ///     the given bytes will be used in reverse order to create the value, such that the endianness of the
+        ///     created value will match the system's.</param>
+        /// <returns>The converted value.</returns>
+        /// <exception cref="IndexOutOfRangeException"><paramref name="bytes"/> does not contain enough bytes.</exception>
+        public unsafe static T To<T>(Span<byte> bytes, Endianness endianness)
+            where T : unmanaged
+        {
+            var ptr = stackalloc byte[sizeof(T)];
+
+            try
+            {
+                if (endianness.MatchesSystemEndianness())
+                {
+                    for (var i = 0; i < sizeof(T); i++)
+                        ptr[i] = bytes[i];
+                }
+                else
+                {
+                    for (var i = 0; i < sizeof(T); i++)
+                        ptr[sizeof(T) - i - 1] = bytes[i];
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                if (bytes.Length < sizeof(T))
+                {
+                    throw new ArgumentException($"Not enough bytes for {typeof(T)} (got {bytes.Length}, " +
+                                                $"expected {sizeof(T)}!", nameof(bytes), e);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return *(T*)ptr;
+        }
+
+        /// <summary>
+        /// Converts a series of bytes into an unmanaged value.
+        /// </summary>
+        /// <typeparam name="T">The unmanaged type to convert the bytes to.</typeparam>
+        /// <param name="bytes">A pointer to the bytes to convert.</param>
+        /// <param name="endianness">The endianness of the given bytes. If it doesn't match the system's endianness,
+        ///     the given bytes will be used in reverse order to create the value, such that the endianness of the
+        ///     created value will match the system's.</param>
+        /// <returns>The converted value.</returns>
+        /// <exception cref="IndexOutOfRangeException"><paramref name="bytes"/> does not contain enough bytes.</exception>
+        public unsafe static T To<T>(byte* bytes, Endianness endianness)
+            where T : unmanaged
+        {
+            var ptr = stackalloc byte[sizeof(T)];
+
+            if (endianness.MatchesSystemEndianness())
+            {
+                for (var i = 0; i < sizeof(T); i++)
+                    ptr[i] = bytes[i];
+            }
+            else
+            {
+                for (var i = 0; i < sizeof(T); i++)
+                    ptr[sizeof(T) - i - 1] = bytes[i];
+            }
+
+            return *(T*)ptr;
+        }
+
+        /// <summary>
+        /// Converts a series of bytes into an unmanaged value.
+        /// </summary>
+        /// <typeparam name="T">The unmanaged type to convert the bytes to.</typeparam>
+        /// <param name="bytes">The bytes to convert.</param>
+        /// <param name="offset">The offset from which to start copying in <paramref name="bytes"/>.</param>
+        /// <param name="endianness">The endianness of the given bytes. If it doesn't match the system's endianness,
+        ///     the given bytes will be used in reverse order to create the value, such that the endianness of the
+        ///     created value will match the system's.</param>
+        /// <returns>The converted value.</returns>
+        /// <exception cref="IndexOutOfRangeException"><paramref name="bytes"/> does not contain enough bytes.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="bytes"/> is null.</exception>
+        public unsafe static T To<T>(byte[] bytes, int offset, Endianness endianness)
+            where T : unmanaged
+        {
+            if (bytes is null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            var ptr = stackalloc byte[sizeof(T)];
+
+            try
+            {
+                if (endianness.MatchesSystemEndianness())
+                {
+                    for (var i = 0; i < sizeof(T); i++)
+                        ptr[i] = bytes[i + offset];
+                }
+                else
+                {
+                    for (var i = 0; i < sizeof(T); i++)
+                        ptr[sizeof(T) - i - 1] = bytes[i + offset];
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                if (bytes.Length - offset < sizeof(T))
+                {
+                    throw new ArgumentException($"Not enough bytes for {typeof(T)} with offset {offset} (got " +
+                                                $"{bytes.Length - offset}, expected {sizeof(T)}!", nameof(bytes), e);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return *(T*)ptr;
+        }
+
+
+        /// <summary>
+        /// Converts a series of bytes into an unmanaged value.
+        /// </summary>
+        /// <typeparam name="T">The unmanaged type to convert the bytes to.</typeparam>
+        /// <param name="bytes">The bytes to convert.</param>
+        /// <param name="endianness">The endianness of the given bytes. If it doesn't match the system's endianness,
+        ///     the given bytes will be used in reverse order to create the value, such that the endianness of the
+        ///     created value will match the system's.</param>
+        /// <returns>The converted value.</returns>
+        /// <exception cref="ArgumentException"><paramref name="bytes"/> does not contain enough bytes.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="bytes"/> is null.</exception>
+        public unsafe static T To<T>(IEnumerable<byte> bytes, Endianness endianness)
+            where T : unmanaged
+        {
+            if (bytes is null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            var ptr = stackalloc byte[sizeof(T)];
+
+            var enumerator = bytes.GetEnumerator();
+            if (endianness.MatchesSystemEndianness())
+            {
+                for (var i = 0; i < sizeof(T); i++)
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        ptr[i] = enumerator.Current;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Not enough bytes for {typeof(T)} (got " +
+                                                    $"{i}, expected {sizeof(T)}!", nameof(bytes));
+                    }
+                }
+            }
+            else
+            {
+                for (var i = 0; i < sizeof(T); i++)
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        ptr[sizeof(T) - i - 1] = enumerator.Current;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Not enough bytes for {typeof(T)} (got " +
+                                                    $"{i}, expected {sizeof(T)}!", nameof(bytes));
+                    }
+                }
+            }
+
+            return *(T*)ptr;
         }
     }
 }
---
+++ HotLib/Bits/Endianness.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HotLib.Bits
+{
+    /// <summary>
+    /// The different byte orders a system may use.
+    /// </summary>
+    public enum Endianness
+    {
+        BigEndian,
+        LittleEndian,
+    }
+
+    /// <summary>
+    /// Contains public static extension methods for <see cref="Endianness"/>.
+    /// </summary>
+    public static class EndiannessExtensions
+    {
+        /// <summary>
+        /// Gets if the given <see cref="Endianness"/> value matches the endianness the current system running the code uses.
+        /// </summary>
+        /// <param name="endianness">The endianness value to check.</param>
+        /// <returns>True if the value matches the system's endianness, false if not.</returns>
+        /// <exception cref="ArgumentException"><paramref name="endianness"/> is not defined in <see cref="Endianness"/>.</exception>
+        public static bool MatchesSystemEndianness(this Endianness endianness)
+        {
+            switch (endianness)
+            {
+                case Endianness.BigEndian:
+                    return !BitConverter.IsLittleEndian;
+                case Endianness.LittleEndian:
+                    return BitConverter.IsLittleEndian;
+                default:
+                    throw new ArgumentException($"Invalid {typeof(Endianness)} value {endianness}!", nameof(endianness));
+            }
+        }
+    }
+}
