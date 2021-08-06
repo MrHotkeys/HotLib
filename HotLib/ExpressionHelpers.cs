3c17840526d904f7b62f1ca7a8aea20b9cab7203
Mr.Hotkeys <mr.hotkeys@send.help>
2021-08-06 18:31:55 -0400

    ExpressionHelper.Throw overloads (throwverloads) to set the type of the throw expression as well as to substitute in other expressions when creating the exception

--- HotLib/ExpressionHelpers.cs
+++ HotLib/ExpressionHelpers.cs
@@ -30,7 +30,173 @@ namespace HotLib
             return Expression.Call(null, ReferenceEqualsMethod, x, y);
         }
 
-        public static Expression Throw<TException>(Expression<Func<TException>> exceptionExpr) => Expression.Throw(exceptionExpr.Body);
+        /// <summary>
+        /// Captures an expression that creates an exception and uses it to create a throw expression.
+        /// </summary>
+        /// <typeparam name="TException">The type of exception that will be thrown.</typeparam>
+        /// <param name="exceptionExpr">The exception creation expression to capture - can be any expression that returns an exception.</param>
+        /// <returns>The created throw expression.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exceptionExpr"/> is null. </exception>
+        public static Expression Throw<TException>(Expression<Func<TException>> exceptionExpr) =>
+            Throw(exceptionExpr, exceptionExpr?.Type ?? typeof(void));
+
+        /// <summary>
+        /// Captures an expression that creates an exception and uses it to create a throw expression.
+        /// </summary>
+        /// <typeparam name="TException">The type of exception that will be thrown.</typeparam>
+        /// <param name="exceptionExpr">The exception creation expression to capture - can be any expression that returns an exception.</param>
+        /// <param name="type">The type to set the throw expresion as.</param>
+        /// <returns>The created throw expression.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exceptionExpr"/> or <paramref name="type"/> is null. </exception>
+        public static Expression Throw<TException>(Expression<Func<TException>> exceptionExpr, Type type) =>
+            exceptionExpr is null ? throw new ArgumentNullException(nameof(exceptionExpr)) :
+            type is null ? throw new ArgumentNullException(nameof(type)) :
+            Expression.Throw(exceptionExpr.Body, type);
+
+        /// <summary>
+        /// Captures an expression that creates an exception and uses it to create a throw expression.
+        /// </summary>
+        /// <remarks>This overload allows for a parameter to be defined by the given expression - the corresponding
+        ///     <see cref="ParameterExpression"/> value of this in the tree will be substituted out for the given expression
+        ///     via <see cref="ExpressionParameterSubstitutionVisitor"/>. This allows for some other data from the expression to
+        ///     be captured and used when throwing the exception.</remarks>
+        /// <typeparam name="T">The type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="TException">The type of exception that will be thrown.</typeparam>
+        /// <param name="exceptionExpr">The exception creation expression to capture - can be any expression that returns an exception.</param>
+        /// <param name="expr">The expression that will be substituted into the created expression over
+        ///     any reference to the parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="type">The type to set the throw expresion as.</param>
+        /// <returns>The created throw expression.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exceptionExpr"/>, <paramref name="expr"/> is null. </exception>
+        public static Expression Throw<TExpr, TException>(Expression<Func<TExpr, TException>> exceptionExpr, Expression expr) =>
+            Throw(exceptionExpr, expr, exceptionExpr?.Type ?? typeof(void));
+
+        /// <summary>
+        /// Captures an expression that creates an exception and uses it to create a throw expression.
+        /// </summary>
+        /// <remarks>This overload allows for a parameter to be defined by the given expression - the corresponding
+        ///     <see cref="ParameterExpression"/> value of this in the tree will be substituted out for the given expression
+        ///     via <see cref="ExpressionParameterSubstitutionVisitor"/>. This allows for some other data from the expression to
+        ///     be captured and used when throwing the exception.</remarks>
+        /// <typeparam name="T">The type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="TException">The type of exception that will be thrown.</typeparam>
+        /// <param name="exceptionExpr">The exception creation expression to capture - can be any expression that returns an exception.</param>
+        /// <param name="expr">The expression that will be substituted into the created expression over
+        ///     any reference to the parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="type">The type to set the throw expresion as.</param>
+        /// <returns>The created throw expression.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exceptionExpr"/>, <paramref name="expr"/>,
+        ///     or <paramref name="type"/> is null. </exception>
+        public static Expression Throw<T, TException>(Expression<Func<T, TException>> exceptionExpr, Expression expr, Type type) =>
+            exceptionExpr is null ? throw new ArgumentNullException(nameof(exceptionExpr)) :
+            expr is null ? throw new ArgumentNullException(nameof(expr)) :
+            type is null ? throw new ArgumentNullException(nameof(type)) :
+            Expression.Throw(ExpressionParameterSubstitutionVisitor.VisitLambda(exceptionExpr, expr), type);
+
+        /// <summary>
+        /// Captures an expression that creates an exception and uses it to create a throw expression.
+        /// </summary>
+        /// <remarks>This overload allows for a number of parameters to be defined by the given expression - the corresponding
+        ///     <see cref="ParameterExpression"/> values of these in the tree will be substituted out for the given expressions
+        ///     via <see cref="ExpressionParameterSubstitutionVisitor"/>. This allows for some other data from the expression to
+        ///     be captured and used when throwing the exception.</remarks>
+        /// <typeparam name="T1">The first type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="T2">The second type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="TException">The type of exception that will be thrown.</typeparam>
+        /// <param name="exceptionExpr">The exception creation expression to capture - can be any expression that returns an exception.</param>
+        /// <param name="expr1">The expression that will be substituted into the created expression over
+        ///     any reference to the first parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="expr2">The expression that will be substituted into the created expression over
+        ///     any reference to the second parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <returns>The created throw expression.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exceptionExpr"/>, <paramref name="expr1"/>,
+        ///     or <paramref name="expr2"/> is null. </exception>
+        public static Expression Throw<T1, T2, TException>(Expression<Func<T1, T2, TException>> exceptionExpr,
+                                                           Expression expr1, Expression expr2) =>
+            Throw(exceptionExpr, expr1, expr2, exceptionExpr?.Type ?? typeof(void));
+
+        /// <summary>
+        /// Captures an expression that creates an exception and uses it to create a throw expression.
+        /// </summary>
+        /// <remarks>This overload allows for a number of parameters to be defined by the given expression - the corresponding
+        ///     <see cref="ParameterExpression"/> values of these in the tree will be substituted out for the given expressions
+        ///     via <see cref="ExpressionParameterSubstitutionVisitor"/>. This allows for some other data from the expression to
+        ///     be captured and used when throwing the exception.</remarks>
+        /// <typeparam name="T1">The first type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="T2">The second type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="TException">The type of exception that will be thrown.</typeparam>
+        /// <param name="exceptionExpr">The exception creation expression to capture - can be any expression that returns an exception.</param>
+        /// <param name="expr1">The expression that will be substituted into the created expression over
+        ///     any reference to the first parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="expr2">The expression that will be substituted into the created expression over
+        ///     any reference to the second parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="type">The type to set the throw expresion as.</param>
+        /// <returns>The created throw expression.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exceptionExpr"/>, <paramref name="expr1"/>,
+        ///     <paramref name="expr2"/>, or <paramref name="type"/> is null. </exception>
+        public static Expression Throw<T1, T2, TException>(Expression<Func<T1, T2, TException>> exceptionExpr,
+                                                           Expression expr1, Expression expr2, Type type) =>
+            exceptionExpr is null ? throw new ArgumentNullException(nameof(exceptionExpr)) :
+            expr1 is null ? throw new ArgumentNullException(nameof(expr1)) :
+            expr2 is null ? throw new ArgumentNullException(nameof(expr2)) :
+            type is null ? throw new ArgumentNullException(nameof(type)) :
+            Expression.Throw(ExpressionParameterSubstitutionVisitor.VisitLambda(exceptionExpr, expr1, expr2), type);
+
+        /// <summary>
+        /// Captures an expression that creates an exception and uses it to create a throw expression.
+        /// </summary>
+        /// <remarks>This overload allows for a number of parameters to be defined by the given expression - the corresponding
+        ///     <see cref="ParameterExpression"/> values of these in the tree will be substituted out for the given expressions
+        ///     via <see cref="ExpressionParameterSubstitutionVisitor"/>. This allows for some other data from the expression to
+        ///     be captured and used when throwing the exception.</remarks>
+        /// <typeparam name="T1">The first type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="T2">The second type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="T3">The third type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="TException">The type of exception that will be thrown.</typeparam>
+        /// <param name="exceptionExpr">The exception creation expression to capture - can be any expression that returns an exception.</param>
+        /// <param name="expr1">The expression that will be substituted into the created expression over
+        ///     any reference to the first parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="expr2">The expression that will be substituted into the created expression over
+        ///     any reference to the second parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="expr3">The expression that will be substituted into the created expression over
+        ///     any reference to the third parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <returns>The created throw expression.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exceptionExpr"/>, <paramref name="expr1"/>,
+        ///     <paramref name="expr2"/>, or <paramref name="expr3"/> is null. </exception>
+        public static Expression Throw<T1, T2, T3, TException>(Expression<Func<T1, T2, T3, TException>> exceptionExpr,
+                                                               Expression expr1, Expression expr2, Expression expr3) =>
+            Throw(exceptionExpr, expr1, expr2, expr3, exceptionExpr?.Type ?? typeof(void));
+
+        /// <summary>
+        /// Captures an expression that creates an exception and uses it to create a throw expression.
+        /// </summary>
+        /// <remarks>This overload allows for a number of parameters to be defined by the given expression - the corresponding
+        ///     <see cref="ParameterExpression"/> values of these in the tree will be substituted out for the given expressions
+        ///     via <see cref="ExpressionParameterSubstitutionVisitor"/>. This allows for some other data from the expression to
+        ///     be captured and used when throwing the exception.</remarks>
+        /// <typeparam name="T1">The first type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="T2">The second type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="T3">The third type of expression that will be substituted into the created expression.</typeparam>
+        /// <typeparam name="TException">The type of exception that will be thrown.</typeparam>
+        /// <param name="exceptionExpr">The exception creation expression to capture - can be any expression that returns an exception.</param>
+        /// <param name="expr1">The expression that will be substituted into the created expression over
+        ///     any reference to the first parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="expr2">The expression that will be substituted into the created expression over
+        ///     any reference to the second parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="expr3">The expression that will be substituted into the created expression over
+        ///     any reference to the third parameter of <paramref name="exceptionExpr"/>.</param>
+        /// <param name="type">The type to set the throw expresion as.</param>
+        /// <returns>The created throw expression.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exceptionExpr"/>, <paramref name="expr1"/>,
+        ///     <paramref name="expr2"/>, <paramref name="expr3"/>, or <paramref name="type"/> is null. </exception>
+        public static Expression Throw<T1, T2, T3, TException>(Expression<Func<T1, T2, T3, TException>> exceptionExpr,
+                                                               Expression expr1, Expression expr2, Expression expr3, Type type) =>
+            exceptionExpr is null ? throw new ArgumentNullException(nameof(exceptionExpr)) :
+            expr1 is null ? throw new ArgumentNullException(nameof(expr1)) :
+            expr2 is null ? throw new ArgumentNullException(nameof(expr2)) :
+            expr3 is null ? throw new ArgumentNullException(nameof(expr3)) :
+            type is null ? throw new ArgumentNullException(nameof(type)) :
+            Expression.Throw(ExpressionParameterSubstitutionVisitor.VisitLambda(exceptionExpr, expr1, expr2, expr3), type);
 
         public static Expression BuildGetterExpression(Expression targetExpr, MemberInfo member)
         {
