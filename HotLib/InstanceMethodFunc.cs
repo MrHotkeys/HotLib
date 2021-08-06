5b834b825d11763ad9635bbebace42412184660f
Mr.Hotkeys <mr.hotkeys@send.help>
2021-08-06 15:57:48 -0400

    Fix return types on InstanceMethodFunc

--- HotLib/InstanceMethodFunc.cs
+++ HotLib/InstanceMethodFunc.cs
@@ -9,7 +9,7 @@ namespace HotLib
     /// <typeparam name="TInstance">The type of object to invoke the encapsulated method on.</typeparam>
     /// <typeparam name="TResult">The value returned when invoked.</typeparam>
     /// <param name="instance">The instance to invoke the encapsulated method on.</param>
-    public delegate void InstanceMethodFunc<in TInstance, out TResult>(TInstance instance);
+    public delegate TResult InstanceMethodFunc<in TInstance, out TResult>(TInstance instance);
 
     /// <summary>
     /// Encapsulates an instance method with one parameter, much like <see cref="Func{T, TResult}"/>,
@@ -20,7 +20,7 @@ namespace HotLib
     /// <typeparam name="TResult">The value returned when invoked.</typeparam>
     /// <param name="instance">The instance to invoke the encapsulated method on.</param>
     /// <param name="arg">The argument to pass to the method call.</param>
-    public delegate void InstanceMethodFunc<in TInstance, in T, out TResult>(TInstance instance, T arg);
+    public delegate TResult InstanceMethodFunc<in TInstance, in T, out TResult>(TInstance instance, T arg);
 
     /// <summary>
     /// Encapsulates an instance method with two parameters, much like <see cref="Func{T1, T2, TResult}"/>,
@@ -33,7 +33,7 @@ namespace HotLib
     /// <param name="instance">The instance to invoke the encapsulated method on.</param>
     /// <param name="arg1">The first argument to pass to the method call.</param>
     /// <param name="arg2">The second argument to pass to the method call.</param>
-    public delegate void InstanceMethodFunc<in TInstance, in T1, in T2, out TResult>(TInstance instance, T1 arg1, T2 arg2);
+    public delegate TResult InstanceMethodFunc<in TInstance, in T1, in T2, out TResult>(TInstance instance, T1 arg1, T2 arg2);
 
     /// <summary>
     /// Encapsulates an instance method with three parameters, much like <see cref="Func{T1, T2, T3, TResult}"/>,
@@ -48,7 +48,7 @@ namespace HotLib
     /// <param name="arg1">The first argument to pass to the method call.</param>
     /// <param name="arg2">The second argument to pass to the method call.</param>
     /// <param name="arg3">The third argument to pass to the method call.</param>
-    public delegate void InstanceMethodFunc<in TInstance, in T1, in T2, in T3, out TResult>(TInstance instance, T1 arg1, T2 arg2, T3 arg3);
+    public delegate TResult InstanceMethodFunc<in TInstance, in T1, in T2, in T3, out TResult>(TInstance instance, T1 arg1, T2 arg2, T3 arg3);
 
     /// <summary>
     /// Encapsulates an instance method with four parameters, much like <see cref="Func{T1, T2, T3, T4, TResult}"/>,
@@ -65,7 +65,7 @@ namespace HotLib
     /// <param name="arg2">The second argument to pass to the method call.</param>
     /// <param name="arg3">The third argument to pass to the method call.</param>
     /// <param name="arg4">The fourth argument to pass to the method call.</param>
-    public delegate void InstanceMethodFunc<in TInstance, in T1, in T2, in T3, in T4, out TResult>(TInstance instance, T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+    public delegate TResult InstanceMethodFunc<in TInstance, in T1, in T2, in T3, in T4, out TResult>(TInstance instance, T1 arg1, T2 arg2, T3 arg3, T4 arg4);
 
     /// <summary>
     /// Encapsulates an instance method with five parameters, much like <see cref="Func{T1, T2, T3, T4, T5, TResult}"/>,
@@ -84,5 +84,5 @@ namespace HotLib
     /// <param name="arg3">The third argument to pass to the method call.</param>
     /// <param name="arg4">The fourth argument to pass to the method call.</param>
     /// <param name="arg5">The fifth argument to pass to the method call.</param>
-    public delegate void InstanceMethodFunc<in TInstance, in T1, in T2, in T3, in T4, in T5, out TResult>(TInstance instance, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+    public delegate TResult InstanceMethodFunc<in TInstance, in T1, in T2, in T3, in T4, in T5, out TResult>(TInstance instance, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
 }
